e1e52f2a463bff3ea94e984dcbf51d8d7f859b73
harugasumi-works <sonngunguoi3@gmail.com>
2025-12-22 00:02:27 +0900

    update doc

--- src/com/harugasumi/core/Task.java
+++ src/com/harugasumi/core/Task.java
@@ -5,7 +5,7 @@ import java.util.concurrent.Callable;
  * Made by @author harugasumi-works
  * このインターフェースは、タスクの基本的な構造を定義します。
  * 各タスクは優先度を持ち、ログ出力のためのロガーを設定できる必要があります。
- * {code: Callable<Boolean>} を拡張しており、タスクの実行結果を示すブール値を返します。
+ * {@code Callable<Boolean>} を拡張しており、タスクの実行結果を示すブール値を返します。
  */
 
 public interface Task extends Callable<Boolean> { 
@@ -15,7 +15,7 @@ public interface Task extends Callable<Boolean> {
       */
    int getPriority();
       /** 
-      * {@code: Consumer<String>} 変数logger を受け取り、タスクのログ出力に使用します。
+      * {@code Consumer<String>} 変数logger を受け取り、タスクのログ出力に使用します。
       * @param logger ログメッセージを受け取るためのコンシューマーを設定します。
       */
    void setLogger(java.util.function.Consumer<String> logger);
